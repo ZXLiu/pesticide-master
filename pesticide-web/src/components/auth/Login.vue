59c17f636aa9dda0f4800a41a977a9f620743e2a
DannLiu <lhlh326@outlook.com>
2023-01-09 19:31:14 +0800

    jjj changed some files of web

--- pesticide-web/src/components/auth/Login.vue
+++ pesticide-web/src/components/auth/Login.vue
@@ -3,7 +3,7 @@
         <div class="container d-flex align-items-center">
             <div class="form-holder has-shadow">
                 <div class="row">
-                    <!-- Logo & Information Panel-->
+
                     <div class="col-lg-6">
                         <div class="info d-flex align-items-center">
                             <div class="content">
@@ -13,7 +13,7 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Form Panel    -->
+
                     <div class="col-lg-6 bg-white">
                         <div class="form d-flex align-items-center">
                             <div class="content">
@@ -24,7 +24,6 @@
                                                name="username" placeholder="用户名"
                                                required type="text"
                                                v-model="userAuth.username">
-                                        <!--<label class="label-material" for="login-username" v-if="!hasData">用户名</label>-->
                                     </div>
                                     <div class="form-group">
                                         <input class="input-material" data-msg="Please enter your password"
@@ -32,14 +31,14 @@
                                                name="password" placeholder="密码"
                                                required type="password"
                                                v-model="userAuth.password">
-                                        <!--<label class="label-material" for="login-password" v-if="!hasData">密码</label>-->
+
                                     </div>
                                     <div class="form-group">
                                         <button @click="login" class="btn btn-primary"
                                                 name="registerSubmit" type="button">登录
                                         </button>
                                     </div>
-                                    <!-- This should be submit button but I replaced it with <a> for demo purposes-->
+
                                 </form>
                                 <a class="forgot-pass" href="#">忘记密码?</a><br>
                                 <small>没有账号?</small>
@@ -50,48 +49,43 @@
                 </div>
             </div>
         </div>
-        <div class="copyrights text-center">
-            <p>Design by <a class="external" href="#">Bootstrapious</a>
-                <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
-            </p>
-        </div>
     </div>
 </template>
 
 <script>
 
-    import AuthService from '../../service/AuthService'
+import AuthService from '../../service/AuthService'
 
-    export default {
-        name: 'Login',
-        mounted () {
-            let userAuth = this.$route.params.userAuth
-            if (userAuth != null) {
-                this.userAuth = userAuth
-                this.hasData = true
-            }
-        },
-        data () {
-            return {
-                userAuth: {
-                    authType: 'USERNAME',
-                    username: '',
-                    password: ''
-                },
-                hasData: false
-            }
-        },
-        methods: {
-            login () {
-                AuthService.login(this.userAuth, data => {
-                    console.log(data)
-                    this.$router.replace({path: '/index'})
-                }, function (code, message) {
-                    alert('登录失败！' + message)
-                })
-            }
+export default {
+    name: 'Login',
+    mounted () {
+        let userAuth = this.$route.params.userAuth
+        if (userAuth != null) {
+            this.userAuth = userAuth
+            this.hasData = true
+        }
+    },
+    data () {
+        return {
+            userAuth: {
+                authType: 'USERNAME',
+                username: '',
+                password: ''
+            },
+            hasData: false
+        }
+    },
+    methods: {
+        login () {
+            AuthService.login(this.userAuth, data => {
+                console.log(data)
+                this.$router.replace({path: '/index'})
+            }, function (code, message) {
+                alert('登录失败！' + message)
+            })
         }
     }
+}
 </script>
 
 <style>
--- pesticide-web/src/components/auth/Register.vue
+++ pesticide-web/src/components/auth/Register.vue
@@ -3,7 +3,7 @@
         <div class="container d-flex align-items-center">
             <div class="form-holder has-shadow">
                 <div class="row">
-                    <!-- Logo & Information Panel-->
+
                     <div class="col-lg-6">
                         <div class="info d-flex align-items-center">
                             <div class="content">
@@ -13,7 +13,7 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Form Panel    -->
+
                     <div class="col-lg-6 bg-white">
                         <div class="form d-flex align-items-center">
                             <div class="content">
@@ -24,7 +24,6 @@
                                                name="registerUsername" placeholder="用户名"
                                                required
                                                type="text" v-model="userAuth.username">
-                                        <!--<label class="label-material" for="register-username">用户名</label>-->
                                     </div>
                                     <div class="form-group">
                                         <input class="input-material" data-msg="Please enter your password"
@@ -32,15 +31,7 @@
                                                placeholder="密码" required
                                                type="password"
                                                v-model="userAuth.password">
-                                        <!--<label class="label-material" for="register-password">密码</label>-->
                                     </div>
-                                    <!--<div class="form-group terms-conditions">-->
-                                        <!--<input class="checkbox-template" data-msg="Your agreement is required"-->
-                                               <!--id="register-agree" name="registerAgree"-->
-                                               <!--required type="checkbox"-->
-                                               <!--value="1">-->
-                                        <!--<label for="register-agree">我已阅读并同意隐私政策</label>-->
-                                    <!--</div>-->
                                     <div class="form-group">
                                         <button @click="register" class="btn btn-primary" id="regidter"
                                                 name="registerSubmit" type="button">注册
@@ -55,41 +46,37 @@
                 </div>
             </div>
         </div>
-        <div class="copyrights text-center">
-            <p>Copyright &copy; 2019. <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect
-                from <a href="http://www.cssmoban.com/" target="_blank" title="网页模板">网页模板</a></p>
-        </div>
     </div>
 </template>
 
-<script>
+<script >
 
-    import AuthService from '../../service/AuthService'
+import AuthService from '../../service/AuthService'
 
-    export default {
-        name: 'Login',
-        data () {
-            return {
-                userAuth: {
-                    authType: 'USERNAME',
-                    username: '',
-                    password: ''
-                }
-            }
-        },
-        methods: {
-            register () {
-                // let self = this
-                AuthService.register(this.userAuth, data => {
-                    alert('注册成功！')
-                    this.$router.replace({name: 'Login', params: {userAuth: this.userAuth}})
-                }, (code, message) => {
-                    console.log('code = ' + code + ', message = ' + message)
-                    alert('注册失败！' + message)
-                })
+export default {
+    name: 'Login',
+    data () {
+        return {
+            userAuth: {
+                authType: 'USERNAME',
+                username: '',
+                password: ''
             }
         }
+    },
+    methods: {
+        register () {
+            // let self = this
+            AuthService.register(this.userAuth, data => {
+                alert('注册成功！')
+                this.$router.replace({name: 'Login', params: {userAuth: this.userAuth}})
+            }, (code, message) => {
+                console.log('code = ' + code + ', message = ' + message)
+                alert('注册失败！' + message)
+            })
+        }
     }
+}
 </script>
 
 <style scoped>
